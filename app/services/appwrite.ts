4c0622a43a8dce193b3e6f6e68a4dfe103d32bfa
Aditya <adiroychaudhuri@gmail.com>
2026-02-16 23:34:32 +0530

    Made 3 new services saveMovie - for saving a movie in database, getSavedMovies - for fetching the save movies from db, and isMovieSaved - to check if the movie is saved or not.

--- app/services/appwrite.ts
+++ app/services/appwrite.ts
@@ -4,6 +4,7 @@ import { Client, TablesDB, Query, ID, Account } from 'react-native-appwrite'
 
 const DB_ID = process.env.EXPO_PUBLIC_APPWRITE_DB_ID!;
 const TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_TABLE_ID!;
+const SAVED_TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_SAVED_TABLE_ID!;
 
 const client = new Client()
     .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
@@ -72,5 +73,77 @@ export const showTrendingMovies = async (): Promise<TrendingMovie[] | undefined>
     } catch (error) {
         console.log(error);
         throw error;
+    }
+}
+
+export const saveMovie = async (userId: string, movie: Movie) => {
+    try {
+        const existing = await db.listRows({
+            databaseId: DB_ID,
+            tableId: SAVED_TABLE_ID,
+            queries: [
+                Query.equal("user_id", userId),
+                Query.equal("movie_id", movie.id)
+            ]
+        })
+
+        if (existing.rows.length > 0){
+            return "Already saved";
+        }
+
+        await db.createRow(
+            DB_ID,
+            SAVED_TABLE_ID,
+            ID.unique(),
+            {
+                user_id: userId,
+                movie_id: movie.id,
+                title: movie.title,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                vote_average: movie.vote_average,
+                release_date: movie.release_date
+            }
+        );
+
+        return 'saved';
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+}
+
+export const getSavedMovies = async (user_id: string) => {
+    try {
+        const res = await db.listRows({
+            databaseId: DB_ID,
+            tableId: SAVED_TABLE_ID,
+            queries: [
+                Query.equal('user_id', user_id),
+                Query.orderDesc("$createdAt")
+            ]
+        })
+
+        return res.rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const isMovieSaved = async (user_id: string, movie: Movie): Promise<boolean | undefined> => {
+    try {
+        const res = await db.listRows({
+            databaseId: DB_ID,
+            tableId: SAVED_TABLE_ID,
+            queries: [
+                Query.equal('user_id', user_id),
+                Query.equal('movie_id', movie.id)
+            ]
+        })
+
+        return res.rows.length > 0;
+    } catch (error) {
+        console.log("Saved error: ", error);
+        return false;
     }
 }
